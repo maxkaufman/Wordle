4bed9be348e2e57644bc1c911c075b08dbac11db
maxkaufman <91966735+maxkaufman@users.noreply.github.com>
2022-06-07 00:15:14 -0400

    Update and rename main.py to sim.py

--- sim.py
+++ sim.py
@@ -5,22 +5,28 @@ for line in f for word in line.split())
 
 
 def guessWord(word):
-    first_guess = "stale"
+    first_guess = "salet"
     letterdict = {}
     strlist = ""
     bestscore = 0
     bestword = ""
     guesses = 1
+    letterinword = 0
+    wordletters = ""
     wordlist = copy.copy()
     newwordlist = []
 
     for x in range(5):
         if first_guess[x] == word[x]:
+            letterinword += 1
+            wordletters += first_guess[x]
             for i in range(len(wordlist)):
                 item = wordlist[i]
                 if len(item) > 1 and first_guess[x] != item[x]:
                     wordlist[i] = "0"
         elif first_guess[x] in word:
+            letterinword += 1
+            wordletters += first_guess[x]
             for i in range(len(wordlist)):
                 item = wordlist[i]
                 if len(item) > 1 and first_guess[x] == item[x]:
@@ -50,11 +56,19 @@ def guessWord(word):
     for z in newwordlist:
         currword = list(z)
         currscore = 0
-        for y in currword:
-            currscore += letterdict[y]
+        for y in range(len(currword)):
+            if currword[y] != currword[y-1]:
+                currscore += letterdict[currword[y]]
+            if currword[y] in wordletters:
+                currscore += 100
+            if currword.count(currword[y]) <= 1:
+                currscore += 10000
+
         if currscore > bestscore:
             bestscore = currscore
             bestword = z
+        if letterinword <= 1:
+            bestword = "choir"
 
     guesses += 1
     while bestword != word:
@@ -102,8 +116,13 @@ def guessWord(word):
         for z in looplist:
             currword = list(z)
             currscore = 0
-            for y in currword:
-                currscore += letterdict[y]
+            for y in range(len(currword)):
+                if currword[y] != currword[y - 1]:
+                    currscore += letterdict[currword[y]]
+                if currword[y] not in "qzj":
+                    currscore += 50
+                if currword.count(currword[y]) <= 1:
+                    currscore += 10
             if currscore > bestscore:
                 bestscore = currscore
                 bestword = z
@@ -111,44 +130,23 @@ def guessWord(word):
     return guesses
 
 def main():
-    count = 0
-    g1 = 0
-    g2 = 0
-    g3 = 0
-    g4 = 0
-    g5 = 0
-    g6 = 0
-    g7 = 0
-    g8 = 0
-    g9 = 0
-    g10 = 0
     total = 0
+    count = 0
+    totaldict = {}
     for i in copy:
+        count += 1
         simguess = guessWord(i)
         total += simguess
-        count += 1
-        if simguess == 1:
-            g1 += 1
-        elif simguess == 2:
-            g2 += 1
-        elif simguess == 3:
-            g3 += 1
-        elif simguess == 4:
-            g4 += 1
-        elif simguess == 5:
-            g5 += 1
-        elif simguess == 6:
-            g6 += 1
-        elif simguess == 7:
-            g7 += 1
-        elif simguess == 8:
-            g8 += 1
-        elif simguess == 9:
-            g9 += 1
-        elif simguess == 10:
-            g10 += 1
+        if simguess in totaldict:
+            totaldict[simguess] += 1
+        else:
+            totaldict[simguess] = 1
+    print(totaldict, total/count)
 
 
-    print('1 -> ' + str(g1) + "\n" + '2 -> ' + str(g2) + "\n" + '3 -> ' + str(g3) + "\n" + '4 -> ' + str(g4) + "\n" + '5 -> ' + str(g5) + "\n" + '6 -> ' + str(g6) + "\n" + '7 -> ' + str(g7) + "\n" + '8 -> ' + str(g8) + "\n" + '9 -> ' + str(g9) + "\n" + '10 -> ' + str(g10) + "\n" + "avg -> " + str(total/count))
 
 main()
+total = 0
+count = 0
+totaldict = {}
+
